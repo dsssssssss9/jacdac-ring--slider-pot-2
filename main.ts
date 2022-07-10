436cb0ef63282053be37b7c644209bc17c49ce18
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-10 21:13:05 +0100

    simplified logic seems to work

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 basic.showNumber(2)
+let Led = 0
+let Old_Led = 0
 basic.forever(function () {
-	
+    Led = Math.round(modules.potentiometer1.position())
+    modules.led1.setPixelColor(Math.map(Old_Led, 0, 100, 0, 7), 0x000000)
+    modules.led1.setPixelColor(Math.map(Led, 0, 100, 0, 7), 0x00ff00)
+    Old_Led = Led
 })
